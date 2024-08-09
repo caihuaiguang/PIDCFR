24b319cc7b4bf1b272db852501e701a1b924fad5
caihuaiguang <1792354623@qq.com>
2024-08-09 19:31:12 +0800

    pidcfr

--- scripts/run_toral.sh
+++ scripts/run_toral.sh
@@ -9,11 +9,11 @@ python scripts/parallel_run.py --algo PDCFRPlus --gamma=5 --alpha=2.3
 python scripts/parallel_run.py --algo PIDCFR
 
 
-python scripts/parallel_run.py --algo CFR --average=False
-python scripts/parallel_run.py --algo CFRPlus --average=False
-python scripts/parallel_run.py --algo LinearCFR --average=False
-python scripts/parallel_run.py --algo DCFR --average=False
-python scripts/parallel_run.py --algo DCFRPlus --gamma=4 --alpha=1.5 --average=False
-python scripts/parallel_run.py --algo PCFRPlus --gamma=2 --average=False
-python scripts/parallel_run.py --algo PDCFRPlus --gamma=5 --alpha=2.3 --average=False
-python scripts/parallel_run.py --algo PIDCFR --average=False
+# python scripts/parallel_run.py --algo CFR --average=False
+# python scripts/parallel_run.py --algo CFRPlus --average=False
+# python scripts/parallel_run.py --algo LinearCFR --average=False
+# python scripts/parallel_run.py --algo DCFR --average=False
+# python scripts/parallel_run.py --algo DCFRPlus --gamma=4 --alpha=1.5 --average=False
+# python scripts/parallel_run.py --algo PCFRPlus --gamma=2 --average=False
+# python scripts/parallel_run.py --algo PDCFRPlus --gamma=5 --alpha=2.3 --average=False
+# python scripts/parallel_run.py --algo PIDCFR --average=False
